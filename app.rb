f2b4ea7557712909efcc60b2671fb7b15e7a8b67
rtboyajian <rtboyajian@gmail.com>
2025-08-05 03:24:17 +0000

    lets commit

--- app.rb
+++ app.rb
@@ -2,8 +2,54 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:homepage)
+end
+
+
+get("/rock") do
+rps_moves = ["rock", "paper", "scissors"]
+
+@comp_move = rps_moves.sample
+
+if @comp_move == "rock"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @outcome = "lost"
+else
+  @outcome = "won"
+end
+
+erb(:rock)
+end
+
+get("/paper") do
+rps_moves = ["rock", "paper", "scissors"]
+
+@comp_move = rps_moves.sample
+
+if @comp_move == "rock"
+  @outcome = "won"
+elsif @comp_move == "paper"
+  @outcome = "tied"
+else
+  @outcome = "lost"
+end
+
+erb(:paper)
+end
+
+get("/scissors") do
+rps_moves = ["rock", "paper", "scissors"]
+
+@comp_move = rps_moves.sample
+
+if @comp_move == "rock"
+  @outcome = "lost"
+elsif @comp_move == "paper"
+  @outcome = "won"
+else
+  @outcome = "tied"
+end
+
+erb(:scissors)
 end
